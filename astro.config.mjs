b3cf5a597cd9c41bfc35236f5d5ec76b5a997299
VINXIS <oykxf2@gmail.com>
2025-01-09 12:29:34 -0700

    add svelte react and vue plugins to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://latex4000.neocities.org", // TODO: Change this to actual domain once decided 
-  integrations: [sitemap()],
+  integrations: [sitemap(), svelte(), react(), vue()],
   build: {
     format: "file",
   },
