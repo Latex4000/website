33c588e613f896f2cf3255d4a11d96dd5bc73ee4
VINXIS <oykxf2@gmail.com>
2025-01-11 23:40:23 -0700

    music page and links

---
+++ src/components/MusicTable.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">    
+interface Song {
+    title: string;
+    youtubeUrl: string;
+    soundcloudUrl: string;
+}
+
+const data: Promise<Song[]> = fetch("https://latex4000.neocities.org/music.json")
+    .then((r) => r.json());
+</script>
+
+{#await data}
+    <p>Loading...</p>
+{:then music}
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>YouTube</th>
+                <th>SoundCloud</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each music as song}
+                <tr>
+                    <td>{song.title}</td>
+                    <td><a href={song.youtubeUrl}>Link</a></td>
+                    <td><a href={song.soundcloudUrl}>Link</a></td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
