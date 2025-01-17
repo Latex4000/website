0050bc9fff75157f5970aefda7744dc532858e0d
VINXIS <oykxf2@gmail.com>
2025-01-16 19:46:24 -0700

    setup words page + content collections + md

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const words = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "src/words" }),
+    schema: z.object({
+        title: z.string(),
+        // Allow dates and parse strings as dates
+        date: z.date().or(z.string().transform((val) => new Date(val))),
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { words };
---
+++ src/words/hello world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+date: 2025-01-16
+---
+
+# Hello
+
+This is the first post!!! Breathtaking
+
+We hope you enjoy what this collective will bring
