dddd24a9064b729c52eb0342323ea45573effad0
clayton <me@clayton.cc>
2025-01-18 22:36:20 -0800

    Alias is so back

--- src/pages/ring.ts
+++ src/pages/ring.ts
@@ -5,37 +5,27 @@ export const prerender = false;
 
 export const GET: APIRoute = async (context) => {
 	const action = context.url.searchParams.get("action");
-	let site = context.url.searchParams.get("from");
+	const alias = context.url.searchParams.get("from");
 
-	if (!site || (action !== "prev" && action !== "next" && action !== "rand"))
+	if (!alias || (action !== "prev" && action !== "next" && action !== "rand"))
 		return new Response(null, { status: 400 });
 
-	try {
-		new URL(site);
-	} catch (e) {
-		// Add https:// to the site and try again
-		try {
-			new URL("https://" + site);
-			site = "https://" + site;
-		} catch (e) {
-			return new Response("Invalid site", { status: 404 });
-		}
-	}
-
 	const members = await db.select({
+		alias: Member.alias,
 		site: Member.site,
-		addedRingToSite: Member.addedRingToSite,
-	}).from(Member).where(eq(Member.addedRingToSite, true));
+	})
+		.from(Member)
+		.where(eq(Member.addedRingToSite, true));
 
 	members.unshift({
+		alias: "Latex 4000",
 		site: context.site?.toString() ?? "",
-		addedRingToSite: true,
 	});
 
-	const fromMemberIndex = members.findIndex((member) => new URL(member.site).toString() === new URL(site).toString());
+	const fromMemberIndex = members.findIndex((member) => member.alias === alias);
 
 	if (fromMemberIndex < 0)
-		return new Response("Invalid site", { status: 404 });
+		return new Response("Invalid alias", { status: 404 });
 
 	let toMemberIndex: number;
 
@@ -54,5 +44,5 @@ export const GET: APIRoute = async (context) => {
 			break;
 	}
 
-	return context.redirect(members[toMemberIndex]?.site ?? '', 307);
+	return context.redirect(members[toMemberIndex]!.site, 307);
 };
