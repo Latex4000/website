fc9d82a879085982ff7fc028f9614ce30c197849
clayton <me@clayton.cc>
2025-01-21 04:00:07 -0800

    Fix RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,22 @@
 import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { db, Word } from "astro:db";
+import { wordId } from "../../db/config";
+
+export const prerender = false;
 
 export const GET: APIRoute = async (context) => {
-    const words = await getCollection("words"); 
+    const words = await db.select().from(Word);
+
     return rss({
         title: "Latex 4000's Words",
         description: "A collection of articles by members in Latex 4000",
-        site: context.site || "",
-        items: words.map(word => ({
-            title: word.data.title,
-            link: `/words/${word.id}`,
-            pubDate: word.data.date,
-            author: word.data.author.replace("\\", ""),
+        site: context.site ?? "",
+        items: words.map((word) => ({
+            title: word.title,
+            link: `/words/${wordId(word)}`,
+            pubDate: word.date,
+            author: "L",
         })),
     })
 }
