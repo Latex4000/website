20887fe26cc5e117b894179baa1d41ab849057a6
clayton <me@clayton.cc>
2025-01-20 23:07:23 -0800

    Check HMAC for members.json

--- src/pages/api/members.json.ts
+++ src/pages/api/members.json.ts
@@ -1,8 +1,14 @@
 import type { APIRoute } from "astro";
 import { db, Member } from "astro:db";
+import checkHmac from "../../server/hmac";
+import { jsonError, jsonResponse } from "../../server/responses";
 
 export const prerender = false;
 
-export const GET: APIRoute = async () => {
-    return new Response(JSON.stringify(await db.select().from(Member)));
+export const GET: APIRoute = async ({ request }) => {
+    if (!checkHmac(request, "")) {
+        return jsonError("Invalid HMAC", 401);
+    }
+
+    return jsonResponse(await db.select().from(Member));
 }
