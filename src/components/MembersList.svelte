32e421756564525b80dc09f4ee96e176cf1a94e7
VINXIS <oykxf2@gmail.com>
2025-01-23 15:58:48 -0700

    css grid and responsive span for members list

--- src/components/MembersList.svelte
+++ src/components/MembersList.svelte
@@ -1,30 +1,64 @@
 <script lang="ts">
-  import type { Member } from "../../db/config";
+import type { Member } from "../../db/config";
 
-  let { members }: { members: Member[] } = $props();
+export let members: Member[] = [];
+
+function gridColumns (node: Node) {
+    const containerWidth = (node as HTMLElement).clientWidth;
+    const lineHeight = parseFloat(window.getComputedStyle(node as HTMLElement).getPropertyValue("--line-height")) * 16; // rem to px
+    const gridColumnWidth = (containerWidth - lineHeight / 2 * 4) / 5;
+    (node as HTMLElement).style.gridTemplateColumns = `repeat(5, minmax(${gridColumnWidth}px, 1fr))`;
+}
+
+function gridSpan (node: Node) {
+    const memberList = document.getElementById("member-list")!;
+    const containerWidth = memberList.clientWidth;
+    const lineHeight = parseFloat(window.getComputedStyle(node as HTMLElement).getPropertyValue("--line-height")) * 16; // rem to px
+    const gridColumnWidth = (containerWidth - lineHeight / 2 * 4) / 5;
+
+    // Change span from more than 1 if text is too long
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d")!;
+    context.font = `${window.getComputedStyle(node as HTMLElement).fontSize} ${window.getComputedStyle(node as HTMLElement).fontFamily}`;
+    // Text measurement + marker
+    const itemWidth = context.measureText((node as HTMLElement).textContent!).width + 16 + 7; // Fucking margin + marker size
+
+    const gridSpan = Math.min(Math.ceil(itemWidth / gridColumnWidth), 5);
+    (node as HTMLElement).style.gridColumn = `span ${gridSpan}`;
+}
 </script>
 
-<div class="member-list">
-  Signed:
-  <ul>
-    {#each members as member}
-      {#if member.addedRingToSite}
-        <li><a href={member.site}>{member.alias}</a></li>
-      {:else}
-        <li>{member.alias}</li>
-      {/if}
-    {/each}
-  </ul>
+<div>
+    Signed:
+    <ul id="member-list" use:gridColumns>
+        {#each members as member}
+            {#if member.addedRingToSite}
+                <li use:gridSpan><a href={member.site}>{member.alias}</a></li>
+            {:else}
+                <li use:gridSpan>{member.alias}</li>
+            {/if}
+        {/each}
+    </ul>
 </div>
 
 <style>
-  ul {
-    margin-top: 0.5rem;
-    column-count: 5;
-  }
-  @media only screen and (max-width: 600px) {
+ul {
+    margin-top: calc(var(--line-height) / 2);
+    padding: 0;
+    display: grid;
+    row-gap: var(--line-height);
+    column-gap: calc(var(--line-height) / 2);
+    width: 100%;
+}
+
+li {
+    white-space: nowrap;
+    list-style-position: inside;
+}
+
+@media only screen and (max-width: 600px) {
     ul {
-      column-count: auto;
+        grid-template-columns: 1fr;
     }
-  }
+}
 </style>
