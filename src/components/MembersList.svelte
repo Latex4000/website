ec71c79f68fd92ed3052e9d2818e94f5a8e43962
VINXIS <oykxf2@gmail.com>
2025-01-23 10:47:24 -0700

    change memberstable to memberslist

---
+++ src/components/MembersList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type { Member } from "../../db/config";
+
+let { members }: { members: Member[] } = $props();
+</script>
+
+<p>
+    {#each members as member, i}
+        {#if member.addedRingToSite}
+            <a href={member.site}>{member.alias}</a>
+        {:else}
+            <span>{member.alias}</span>
+        {/if}
+        
+        {#if i < members.length - 1}|&nbsp; {/if}
+    {/each}
+</p>
