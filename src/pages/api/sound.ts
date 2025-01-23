9d2f935dfe2d27db644f53f1d0bcab71c5d56920
clayton <me@clayton.cc>
2025-01-22 23:16:23 -0800

    Don't take sound date from req params

--- src/pages/api/sound.ts
+++ src/pages/api/sound.ts
@@ -8,7 +8,7 @@ export const prerender = false;
 export const POST: APIRoute = async ({ request }) => {
     try {
         if (request.headers.get("content-type") !== "application/json")
-            throw new Error("Invalid content type");
+            return jsonError("Invalid content type");
 
         if (!await validateHmac(request)) {
             return hmacInvalidResponse();
@@ -20,19 +20,14 @@ export const POST: APIRoute = async ({ request }) => {
             !sound.title ||
             !sound.youtubeUrl ||
             !sound.soundcloudUrl ||
-            !sound.date ||
             typeof sound.title !== "string" ||
             typeof sound.youtubeUrl !== "string" ||
-            typeof sound.soundcloudUrl !== "string" ||
-            typeof sound.date !== "string" ||
-            isNaN(Date.parse(sound.date))
+            typeof sound.soundcloudUrl !== "string"
         )
-            throw new Error("Member has missing string keys/invalid keys");
+            return jsonError("Member has missing string keys/invalid keys");
 
-        sound.date = new Date(sound.date);
-
-        const soundRes = await db.insert(Sound).values([sound]).returning();
-        return jsonResponse(soundRes);
+        await db.insert(Sound).values(sound);
+        return jsonResponse(null);
     } catch (err: any) {
         if (typeof err.status === "number")
             return jsonError(err.message, err.status);
