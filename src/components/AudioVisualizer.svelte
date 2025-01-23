1c9d0ac9c0131e298d4be7227d7af3cc6498764f
VINXIS <oykxf2@gmail.com>
2025-01-23 11:08:21 -0700

    change the look of the instructionnote and show mousepos freq

--- src/components/AudioVisualizer.svelte
+++ src/components/AudioVisualizer.svelte
@@ -19,6 +19,7 @@ let dataArrayRight: Uint8Array;
 let animationId: number;
 let isPlaying = false;
 let isPaused = false;
+let mouseOnVisualizer = 0;
 let fileInput: HTMLInputElement;
 
 let canvasSize: number = 1000;
@@ -132,6 +133,7 @@ const stopAudio = () => {
     ctx.fillRect(0, 0, canvasSize, canvasSize);
     isPlaying = false;
     isPaused = false;
+    mouseOnVisualizer = 0;
 };
 
 const handleKeyDown = (event: KeyboardEvent) => {
@@ -209,10 +211,12 @@ const drawInstructionNote = (ctx: CanvasRenderingContext2D) => {
     ctx.textAlign = "left";
     ctx.font = "10px JetBrains Mono";
     ctx.textBaseline = "top";
-    ctx.fillText("` | x", 2, canvasSize - 12);
-    ctx.fillText("[] | s", 2, canvasSize - 24);
-    ctx.fillText("+/- | v", 2, canvasSize - 36);
-    ctx.fillText("' ' | p", 2, canvasSize - 48);
+    if (mouseOnVisualizer) // Write the frequency the cursor y position is currently at
+        ctx.fillText(frequencyScale.invert(canvasSize - mouseOnVisualizer).toFixed(0), 2, 2);
+    ctx.fillText("`   | x", 2, canvasSize - 48);
+    ctx.fillText(`[]  | ${blockSize}`, 2, canvasSize - 36);
+    ctx.fillText(`+/- | ${gainNode.gain.value.toFixed(1)}`, 2, canvasSize - 24);
+    ctx.fillText(`' ' | ${!isPaused ? "o" : "s"}`, 2, canvasSize - 12);
 };
 
 const handleCanvasClick = () => fileInput.click();
@@ -259,7 +263,10 @@ const mouseAnimation = (timestamp: number) => {
         progress = target;
 };
 
-const onCanvasMouseMove = () => {
+const onCanvasMouseMove = (event: MouseEvent | TouchEvent) => {
+    if (isPlaying) // Canvas top is 0, bottom is canvasSize
+        mouseOnVisualizer = (event instanceof MouseEvent ? event.clientY : event.touches[0]!.clientY) - canvas.offsetTop;
+
     if (isPlaying || target === 1)
         return;
 
@@ -269,6 +276,8 @@ const onCanvasMouseMove = () => {
 };
 
 const onCanvasMouseLeave = () => {
+    mouseOnVisualizer = 0;
+
     if (isPlaying || target === 0)
         return;
 
