0b4059886c8cf299d2033502d451c8c0fd2cf93b
VINXIS <oykxf2@gmail.com>
2025-01-23 19:30:34 -0700

    ssr random and dont redirect to same page

--- src/pages/random.ts
+++ src/pages/random.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 const urls = [
     "/",
     "/sounds",
@@ -8,6 +10,8 @@ const urls = [
 ]
 
 export const GET: APIRoute = (context) => {
-    const randomUrl = urls[Math.floor(Math.random() * urls.length)]!;
+    const ref = context.request.headers.get("referer");
+    const urlsWithoutRef = urls.filter(url => ref !== `${context.url.origin}${url}`);
+    const randomUrl = urlsWithoutRef[Math.floor(Math.random() * urlsWithoutRef.length)]!;
     return context.redirect(randomUrl, 307);
 };
