595d950d397a0f0711f7adf425953f36b88c8ace
VINXIS <oykxf2@gmail.com>
2025-01-27 02:19:04 -0700

    motions api endpoint

---
+++ src/pages/api/motions.ts
@@ -0,0 +1,56 @@
+import type { APIRoute } from "astro";
+import { jsonError, jsonResponse } from "../../server/responses";
+import { db, isDbError, Motion } from "astro:db";
+import { motionFromDb, type MotionType } from "../../../db/config";
+
+export const prerender = false;
+
+export const POST: APIRoute = async ({ request }) => {
+    if (request.headers.get("content-type") !== "application/json")
+        return jsonError("Invalid content type");
+
+    const motionData = await request.json();
+
+    if (
+        !motionData.title ||
+        !motionData.youtubeUrl ||
+        !motionData.memberDiscord ||
+        !motionData.tags ||
+        typeof motionData.title !== "string" ||
+        typeof motionData.youtubeUrl !== "string" ||
+        typeof motionData.memberDiscord !== "string" ||
+        !URL.canParse(motionData.youtubeUrl) ||
+        !Array.isArray(motionData.tags) ||
+        !motionData.tags.every((tag: unknown) => typeof tag === "string")
+    )
+        return jsonError("Invalid motion data");
+
+    if (motionData.tags.join("").length > 2 ** 10)
+        return jsonError("Tags are too long");
+
+    // Store to DB
+    let motion: MotionType;
+    try {
+        motion = motionFromDb(
+            await db
+                .insert(Motion)
+                .values({
+                    title: motionData.title,
+                    youtubeUrl: motionData.youtubeUrl,
+                    memberDiscord: motionData.memberDiscord,
+                    tags: motionData.tags,
+                })
+                .returning()
+                .get(),
+        );
+    } catch (error) {
+        if (isDbError(error) && error.code === "SQLITE_CONSTRAINT_FOREIGNKEY")
+            return jsonError(
+                "Invalid Discord ID; member does not exist; probably needs to join first",
+            );
+
+        throw error;
+    }
+
+    return jsonResponse(motion);
+};
