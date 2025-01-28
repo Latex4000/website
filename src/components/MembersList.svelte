6d5a9ea0cf292b816b7524455d29c0dfcece5b3f
VINXIS <oykxf2@gmail.com>
2025-01-28 15:33:13 -0700

    fix memberlist colcount not changing on smaller widths

--- src/components/MembersList.svelte
+++ src/components/MembersList.svelte
@@ -5,17 +5,26 @@
     let columnCount = 5;
 
     function gridColumns(node: Node) {
+        columnCount = Math.floor(
+            Math.min(5, Math.max(2, 0.0332942 * window.innerWidth - 18.60778)),
+        ); // Dude idk look at this https://www.desmos.com/calculator/cvs8zridx4
+
         const containerWidth = (node as HTMLElement).clientWidth;
         const lineHeight =
             parseFloat(
-                window
-                    .getComputedStyle(node as HTMLElement)
-                    .getPropertyValue("--line-height"),
-            ) * 16; // rem to px
+                getComputedStyle(node as HTMLElement).getPropertyValue(
+                    "--line-height",
+                ),
+            ) * parseFloat(getComputedStyle(document.body).fontSize); // rem to px
         const gridColumnWidth =
             (containerWidth - (lineHeight / 2) * 4) / columnCount;
+
         (node as HTMLElement).style.gridTemplateColumns =
             `repeat(${columnCount}, minmax(${gridColumnWidth}px, 1fr))`;
+
+        const children = document.getElementById("member-list")!.children;
+        for (let i = 0; i < children.length; i++)
+            if (children[i]) gridSpan(children[i]!);
     }
 
     function gridSpan(node: Node) {
@@ -23,22 +32,21 @@
         const containerWidth = memberList.clientWidth;
         const lineHeight =
             parseFloat(
-                window
-                    .getComputedStyle(node as HTMLElement)
-                    .getPropertyValue("--line-height"),
-            ) * 16; // rem to px
+                getComputedStyle(node as HTMLElement).getPropertyValue(
+                    "--line-height",
+                ),
+            ) * parseFloat(getComputedStyle(document.body).fontSize); // rem to px
         const gridColumnWidth =
             (containerWidth - (lineHeight / 2) * 4) / columnCount;
 
         // Change span from more than 1 if text is too long
         const canvas = document.createElement("canvas");
         const context = canvas.getContext("2d")!;
-        context.font = `${window.getComputedStyle(node as HTMLElement).fontSize} ${window.getComputedStyle(node as HTMLElement).fontFamily}`;
+        context.font = `${getComputedStyle(node as HTMLElement).fontSize} ${getComputedStyle(node as HTMLElement).fontFamily}`;
         // Text measurement + marker
         const itemWidth =
             context.measureText((node as HTMLElement).textContent!).width +
-            16 +
-            7; // Fucking margin + marker size
+            parseFloat(getComputedStyle(document.body).fontSize) * 1.4375; // Fucking margin + marker size
 
         const gridSpan = Math.min(
             Math.ceil(itemWidth / gridColumnWidth),
@@ -48,15 +56,12 @@
     }
 
     // on window resize
-    window.addEventListener("resize", () => {
-        columnCount = Math.floor(
-            Math.min(5, Math.max(2, 0.0332942 * window.innerWidth - 18.60778)),
-        ); // Dude idk look at this https://www.desmos.com/calculator/cvs8zridx4
-        gridColumns(document.getElementById("member-list")!);
-    });
-    const bodyResizeObserver = new ResizeObserver(() => {
-        gridColumns(document.getElementById("member-list")!);
-    });
+    window.addEventListener("resize", () =>
+        gridColumns(document.getElementById("member-list")!),
+    );
+    const bodyResizeObserver = new ResizeObserver(() =>
+        gridColumns(document.getElementById("member-list")!),
+    );
     bodyResizeObserver.observe(document.body);
 </script>
 
