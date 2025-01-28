6a8389fe465f45d77db7c0b75b2dad04339d71f3
clayton <me@clayton.cc>
2025-01-28 03:11:18 -0800

    Set json content type response headers

--- src/server/responses.ts
+++ src/server/responses.ts
@@ -1,26 +1,28 @@
 export function jsonError(message: string, status = 400): Response {
-    return new Response(JSON.stringify({ error: message }), { status });
+    return jsonResponse({ error: message }, status);
 }
 
 export function jsonResponse(data: unknown, status = 200): Response {
-    return new Response(JSON.stringify(data), { status });
+    return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" }, status });
 }
 
 export class ResponseError extends Error {
     readonly response: Response;
 
-    constructor(message?: string, status = 400) {
-      super();
+    constructor(message?: Response | string, status = 400) {
+        super();
 
-      this.response = new Response(message, { status });
+        this.response = message instanceof Response
+            ? message
+            : new Response(message ?? "", { status });
 
-      Object.setPrototypeOf(this, ResponseError.prototype);
+        Object.setPrototypeOf(this, ResponseError.prototype);
     }
 }
 
 export class JsonResponseError extends ResponseError {
     constructor(message: string, status = 400) {
-        super(JSON.stringify({ error: message }), status);
+        super(jsonError(message, status));
 
         Object.setPrototypeOf(this, JsonResponseError.prototype);
     }
