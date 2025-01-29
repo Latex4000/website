9fe846d4af55631b354a18d2128c64a7c152e9a7
VINXIS <oykxf2@gmail.com>
2025-01-29 13:45:30 -0700

    change nottheme settings for 1 dropdown

--- src/styles/nottheme.ts
+++ src/styles/nottheme.ts
@@ -2,50 +2,45 @@ import type { Settings } from "nottheme";
 
 export default {
     options: {
-        "color-scheme": {
+        "themes": {
             name: "",
-            choices: ["light", "dark"],
+            choices: ["mono-light", "mono-dark", "cs16"],
             values: {
-                light: {
+                "mono-light": {
+                    "--line-height": "1.2rem",
                     "--text-color": "#000",
                     "--text-color-alt": "#666",
                     "--background-color": "#fff",
                     "--background-color-alt": "#eee",
+                    "--srclink": "https://owickstrom.github.io/the-monospace-web/",
+                    "--srctext": "Design implemented from The Monospace Web",
+                    "--srcopacity": "0.5",
                 },
-                dark: {
+                "mono-dark": {
+                    "--line-height": "1.2rem",
                     "--text-color": "#fff",
                     "--text-color-alt": "#ccc",
                     "--background-color": "#000",
                     "--background-color-alt": "#111",
-                },
-            },
-            default: [
-                { query: "(prefers-color-scheme: dark)", choice: "dark" },
-                "light",
-            ]
-        },
-        "style": {
-            name: "",
-            choices: ["mono", "cs16"],
-            values: {
-                mono: {
                     "--srclink": "https://owickstrom.github.io/the-monospace-web/",
                     "--srctext": "Design implemented from The Monospace Web",
                     "--srcopacity": "0.5",
-                    "--line-height": "1.2rem",
                 },
-                cs16: {
-                    "--srclink": "https://cs16.samke.me/",
-                    "--srctext": "Design implemented from cs16.css",
-                    "--srcopacity": "1",
+                "cs16": {
                     "--line-height": "1.5rem",
                     "--background-color": "#4a5942",
                     "--background-color-alt": "#3e4637",
                     "--text-color": "#dedfd6",
                     "--text-color-alt": "#d8ded3",
+                    "--srclink": "https://cs16.samke.me/",
+                    "--srctext": "Design implemented from cs16.css",
+                    "--srcopacity": "1",
                 },
             },
-            default: "mono"
-        }
+            default: [
+                { query: "(prefers-color-scheme: dark)", choice: "mono-dark" },
+                "mono-light",
+            ]
+        },
     }
 } satisfies Settings;
