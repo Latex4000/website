b6d2465e8be0d7c3dc86afd6a8d283564cdfd8e8
clayton <me@clayton.cc>
2025-01-29 15:50:42 -0800

    Upload migration files on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -eu
 ./build.sh
 
 rsync -lprv --delete dist/client/ nonacademic.net:/srv/latex/
-rsync -lprv --delete --relative dist/server/ .nvmrc package.json package-lock.json nonacademic.net:latex-server/
+rsync -lprv --delete --relative db/migration/ db/migrate.mjs dist/server/ .nvmrc package.json package-lock.json nonacademic.net:latex-server/
 
 ssh nonacademic.net ./latex-server-restart
