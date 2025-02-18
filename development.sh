b2dbc2d1abdf6afd56b4ef63e60ea738ea8d7cb7
clayton <me@clayton.cc>
2025-02-18 11:46:48 -0800

    Order

--- development.sh
+++ development.sh
@@ -6,21 +6,21 @@ database='dev/latex.db'
 
 export DATABASE_URL="file:./$database"
 export SECRET_HMAC_KEY='dev'
+export SIGHTS_RUN_AFTER_UPLOAD=
+export SIGHTS_UPLOAD_DIRECTORY='dev/sights'
 export SOUNDS_RUN_AFTER_UPLOAD=
 export SOUNDS_UPLOAD_DIRECTORY='dev/sounds'
 export WORDS_RUN_AFTER_UPLOAD=
 export WORDS_UPLOAD_DIRECTORY='dev/words'
-export SIGHTS_RUN_AFTER_UPLOAD=
-export SIGHTS_UPLOAD_DIRECTORY='dev/sights'
 export PUBLIC_DIRECTORY='public'
 
 # Clean up files from last run
-rm -rf "$database" "$SOUNDS_UPLOAD_DIRECTORY" "$WORDS_UPLOAD_DIRECTORY" "$SIGHTS_UPLOAD_DIRECTORY"
+rm -rf "$database" "$SIGHTS_UPLOAD_DIRECTORY" "$SOUNDS_UPLOAD_DIRECTORY" "$WORDS_UPLOAD_DIRECTORY"
 
 # Create directories
+mkdir -p "$SIGHTS_UPLOAD_DIRECTORY"
 mkdir -p "$SOUNDS_UPLOAD_DIRECTORY"
 mkdir -p "$WORDS_UPLOAD_DIRECTORY"
-mkdir -p "$SIGHTS_UPLOAD_DIRECTORY"
 
 # Create database
 node db/migrate.mjs "$database"
