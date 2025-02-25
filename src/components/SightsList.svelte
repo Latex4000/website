31e8222943bc4b47fd718a21252d3ce325ce8a80
clayton <me@clayton.cc>
2025-02-24 23:35:47 -0800

    Slight gray on overlay bg for dark themes

--- src/components/SightsList.svelte
+++ src/components/SightsList.svelte
@@ -226,7 +226,7 @@
         left: 0;
         right: 0;
         bottom: 0;
-        background: rgba(0, 0, 0, 0.8);
+        background-color: #222c;
         display: flex;
         align-items: center;
         justify-content: center;
