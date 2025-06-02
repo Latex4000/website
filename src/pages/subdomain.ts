cf4b2c61cfce248e6ce1ac1a5aaf4e17e3c22f80
clayton <me@clayton.cc>
2025-06-02 03:21:14 -0700

    Fix subdomain redirect for nonalphanumericlowercase alias

--- src/pages/subdomain.ts
+++ src/pages/subdomain.ts
@@ -1,9 +1,15 @@
 import type { APIRoute } from "astro";
-import { eq } from "drizzle-orm";
+import { isNotNull } from "drizzle-orm";
 import db from "../database/db";
 import { Member } from "../database/schema";
 
 export const prerender = false;
+
+const memberAliasToHostName = (alias: string) => alias
+    .toLowerCase()
+    .replace(/[^a-z0-9-_]/g, "")
+    .replace(/^-+|-+$/g, "")
+    .slice(0, 63);
 
 export const GET: APIRoute = async (context) => {
     const alias = context.request.headers.get("X-Latex-Alias");
@@ -12,15 +18,17 @@ export const GET: APIRoute = async (context) => {
         return new Response(null, { status: 400 });
     }
 
-    const member = await db
-        .select({ site: Member.site })
+    const hostname = memberAliasToHostName(alias);
+    const members = await db
+        .select({ alias: Member.alias, site: Member.site })
         .from(Member)
-        .where(eq(Member.alias, alias))
-        .get();
+        .where(isNotNull(Member.site));
 
-    if (member?.site == null) {
-        return new Response(null, { status: 404 });
+    for (const member of members) {
+        if (hostname === memberAliasToHostName(member.alias)) {
+            return context.redirect(member.site!, 307);
+        }
     }
 
-    return context.redirect(member.site, 307);
+    return new Response(null, { status: 404 });
 };
