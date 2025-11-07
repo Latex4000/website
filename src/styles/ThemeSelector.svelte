7681e2e501546ff09f63f9f1722c8b9044880948
clayton <me@clayton.cc>
2025-11-07 14:24:00 -0800

    Don't set theme storage on load

--- src/styles/ThemeSelector.svelte
+++ src/styles/ThemeSelector.svelte
@@ -6,15 +6,13 @@
         import.meta.env.SSR ? defaultTheme.slug : document.body.dataset.theme!,
     );
 
-    $effect(() => {
-        applyTheme(themeSlug);
-        if (typeof window !== "undefined") {
-            window.localStorage.setItem("theme-slug", themeSlug);
-        }
-    });
+    $effect(() => applyTheme(themeSlug));
 </script>
 
-<select bind:value={themeSlug}>
+<select
+    bind:value={themeSlug}
+    onchange={() => window.localStorage.setItem("theme-slug", themeSlug)}
+>
     {#each themes as theme}
         <option value={theme.slug}>
             {theme.name}
