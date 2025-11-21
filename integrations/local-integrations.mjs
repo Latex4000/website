116cfa75ae7611715af1f3f4a569a871aaaa4486
clayton <me@clayton.cc>
2025-11-21 01:46:52 -0800

    Build local integrations only when source files are modified

--- integrations/local-integrations.mjs
+++ integrations/local-integrations.mjs
@@ -1,12 +1,12 @@
 import { execFileSync } from "node:child_process";
-import { access, readdir } from "node:fs/promises";
-import { join, resolve } from "node:path";
+import { createHash } from "node:crypto";
+import { access, lstat, readdir, readFile, writeFile } from "node:fs/promises";
+import { basename, join, resolve } from "node:path";
 
 /** @typedef {import("astro").AstroIntegration} AstroIntegration */
 
 /**
- * Build and include local Astro integrations
- *
+ * Build and include local Astro integrations.
  * @returns {AstroIntegration}
  */
 export default function localIntegrations() {
@@ -19,8 +19,11 @@ export default function localIntegrations() {
 }
 
 /** @type {AstroIntegration["hooks"]["astro:config:setup"]} */
-const hook = async ({ command, logger, updateConfig }) => {
-    const integrationEntries = await readdir("integrations", { withFileTypes: true });
+const hook = async ({ createCodegenDir, logger, updateConfig }) => {
+    const codegenDir = createCodegenDir().pathname;
+    const integrationEntries = await readdir("integrations", {
+        withFileTypes: true,
+    });
 
     for (const integrationEntry of integrationEntries) {
         if (!integrationEntry.isDirectory()) {
@@ -28,27 +31,104 @@ const hook = async ({ command, logger, updateConfig }) => {
         }
 
         const integrationPath = join("integrations", integrationEntry.name);
+        const integrationSrcHashPath = join(
+            codegenDir,
+            `src-hash-${integrationEntry.name}`,
+        );
 
-        if (await access(join(integrationPath, "tsconfig.json")).catch(() => true)) {
-            continue;
-        }
-
-        if (command === "build" || await access(join(integrationPath, "dist")).catch(() => true)) {
+        if (
+            await integrationNeedsBuild(integrationPath, integrationSrcHashPath)
+        ) {
             logger.info(`Building "${integrationEntry.name}"`);
 
             execFileSync("tsc", { cwd: integrationPath, stdio: "inherit" });
+
+            await writeFile(
+                integrationSrcHashPath,
+                await hashIntegrationSrc(integrationPath),
+            );
         }
 
         try {
             /** @type {{ default: () => AstroIntegration }} */
-            const integrationModule = await import(join(resolve(integrationPath), "dist/index.js"));
+            const integrationModule = await import(
+                join(resolve(integrationPath), "dist/index.js")
+            );
             const integration = integrationModule.default();
 
             updateConfig({ integrations: [integration] });
 
             logger.info(`Loaded ${integration.name}`);
         } catch {
-            logger.error(`Failed to load "${integrationEntry.name}", make sure the default export of ${join(integrationPath, "index.ts")} is of type () => AstroIntegration`);
+            logger.error(
+                `Failed to load "${integrationEntry.name}", make sure the default export of ${join(integrationPath, "index.ts")} is of type () => AstroIntegration`,
+            );
         }
     }
 };
+
+/**
+ * @param {string} path
+ * @param {string} srcHashPath
+ * @returns {Promise<boolean>}
+ */
+async function integrationNeedsBuild(path, srcHashPath) {
+    if (await access(join(path, "tsconfig.json")).catch(() => true)) {
+        return false;
+    }
+
+    if (await access(srcHashPath).catch(() => true)) {
+        return true;
+    }
+
+    return (
+        (await readFile(srcHashPath, "utf8")) !==
+        (await hashIntegrationSrc(path))
+    );
+}
+
+/**
+ * Get a hash of an integration directory using its source files' paths and modification times.
+ * @param {string} path
+ * @returns {Promise<string>}
+ */
+async function hashIntegrationSrc(path) {
+    const hash = createHash("md5");
+    const stats = await collectStats(path);
+
+    for (const stat of stats) {
+        if (stat.path === path) {
+            continue;
+        }
+
+        hash.update(stat.path);
+        hash.update(stat.mtime.toString(16));
+    }
+
+    return hash.digest("hex");
+
+    /**
+     * @param {string} path
+     * @returns {Promise<{ path: string; mtime: bigint }[]>}
+     */
+    async function collectStats(path) {
+        if (basename(path) === "dist") {
+            return [];
+        }
+
+        const stats = await lstat(path, { bigint: true });
+
+        if (stats.isDirectory()) {
+            const paths = (await readdir(path)).map((filename) =>
+                join(path, filename),
+            );
+            const childStats = await Promise.all(paths.map(collectStats));
+
+            return [{ path, mtime: stats.mtimeMs }, ...childStats.flat()];
+        } else if (stats.isFile()) {
+            return [{ path, mtime: stats.mtimeMs }];
+        }
+
+        throw new Error();
+    }
+}
