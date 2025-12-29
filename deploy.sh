8cb1f249f59c87fa1b86fb425dc83d7246b22761
clayton <me@clayton.cc>
2025-12-28 23:28:28 -0800

    Update deploy paths

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ export CORPORATE_URL=https://corp.nonacademic.net
 
 rm -rf public/emoji
 
-rsync -lprv --delete dist/client/ nonacademic.net:/srv/latex/
-rsync -lprv --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:latex-server/
+rsync -lprv --delete dist/client/ nonacademic.net:/srv/nonacademic/
+rsync -lprv --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:/opt/nonacademic/server/
 
-ssh nonacademic.net 'rsync -lprv --delete latex-emoji/ /srv/latex/emoji/'
-ssh nonacademic.net ./latex-server-restart
+ssh nonacademic.net 'rsync -lprv --delete /opt/nonacademic/emoji/ /srv/nonacademic/emoji/'
+ssh nonacademic.net /opt/nonacademic/server-restart
