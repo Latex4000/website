aed2ddc6797d685acdaa740f368a1a4d89eee728
clayton <me@clayton.cc>
2026-01-17 11:16:03 -0800

    More deploy paths+permissions fix

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ export CORPORATE_URL=https://corp.nonacademic.net
 
 rm -rf public/emoji
 
-rsync -lprv --delete dist/client/ nonacademic.net:/srv/nonacademic/
-rsync -lprv --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:/opt/nonacademic/server/
+rsync -lrv --delete dist/client/ nonacademic.net:/srv/nonacademic/www/
+rsync -lrv --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:/opt/nonacademic/server/
 
-ssh nonacademic.net 'rsync -lprv --delete /opt/nonacademic/emoji/ /srv/nonacademic/emoji/'
+ssh nonacademic.net 'rsync -lrv --delete /opt/nonacademic/emoji/ /srv/nonacademic/www/emoji/'
 ssh nonacademic.net /opt/nonacademic/server-restart
