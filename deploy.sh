ac8f9888797b0b5e418291726fda0ad0aa0dcccb
clayton <me@clayton.cc>
2026-02-02 16:32:46 -0800

    Fix deploy permissions More
    Setting --chmod to all bits like this lets the destination use its ACL
    with no other restrictions

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ export CORPORATE_URL=https://corp.nonacademic.net
 
 rm -rf public/emoji
 
-rsync -lrv --delete dist/client/ nonacademic.net:/srv/nonacademic/www/
-rsync -lrv --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:/opt/nonacademic/server/
+rsync -lrv --chmod=ugo=rwX --delete dist/client/ nonacademic.net:/srv/nonacademic/www/
+rsync -lrv --chmod=ugo=rwX --delete --relative db/migration/ db/migrate.mjs db/cron.mjs dist/server/ mise.toml package.json package-lock.json nonacademic.net:/opt/nonacademic/server/
 
-ssh nonacademic.net 'rsync -lrv --delete /opt/nonacademic/emoji/ /srv/nonacademic/www/emoji/'
+ssh nonacademic.net 'rsync -lrv --chmod=ugo=rwX --delete /opt/nonacademic/emoji/ /srv/nonacademic/www/emoji/'
 ssh nonacademic.net /opt/nonacademic/server-restart
