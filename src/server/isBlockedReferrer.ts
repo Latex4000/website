62cb2698294d7af24023dbb4ba8da2d3b59458c2
VINXIS <oykxf2@gmail.com>
2026-02-27 06:10:03 -0700

    new ref host to block

--- src/server/isBlockedReferrer.ts
+++ src/server/isBlockedReferrer.ts
@@ -1,5 +1,8 @@
 
-const BLOCKED_REFERRER_HOSTS = ["muloves.ru"];
+const BLOCKED_REFERRER_HOSTS = [
+    "muloves.ru",
+    "dom2-fany.ru",
+];
 
 export default function isBlockedReferrerHost(host: string): boolean {
     return BLOCKED_REFERRER_HOSTS.some((blocked) =>
